71dabba3f29949a3ca620866d72aa8d46edf3b08
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-21 10:31:01 +0200

    Edited the code in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import StudentDashboard from "./pages/Dashboard/StudentDashboard";
+import InstructorDashboard from "./pages/Dashboard/InstructorDashboard";
+import AdminDashboard from "./pages/Dashboard/AdminDashboard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/student-dashboard" element={<StudentDashboard />} />
+                <Route path="/instructor-dashboard" element={<InstructorDashboard />} />
+                <Route path="/admin-dashboard" element={<AdminDashboard />} />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
