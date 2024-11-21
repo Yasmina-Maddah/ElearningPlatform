9e0891604cce68b3a850b7af837552f565704c7b
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-21 11:31:47 +0200

    fetched the courses service api

---
+++ src/api/courseService.js
@@ -0,0 +1,28 @@
+import api from "./api"; // Import the Axios instance
+
+export const getAllCourses = async () => {
+    const response = await api.get("/courses/getAllCourses.php");
+    return response.data;
+};
+
+export const getCourseDetails = async (courseId) => {
+    const response = await api.get(`/courses/getCourseDetails.php?courseId=${courseId}`);
+    return response.data;
+};
+
+export const createCourse = async (courseData) => {
+    const response = await api.post("/courses/createCourse.php", courseData);
+    return response.data;
+};
+
+export const editCourse = async (courseData) => {
+    const response = await api.put("/courses/editCourse.php", courseData);
+    return response.data;
+};
+
+export const deleteCourse = async (courseId) => {
+    const response = await api.delete("/courses/deleteCourse.php", {
+        data: { courseId },
+    });
+    return response.data;
+};
