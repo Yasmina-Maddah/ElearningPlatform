e7c4aa0b63d7dcc05cc401cd53ca97524a606a76
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-21 11:26:59 +0200

    Updated the App.js file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
-import StudentDashboard from "./pages/Dashboard/StudentDashboard";
+import StudentDashboard from "./components/Student/Dashboard";
+import BrowseCourses from "./components/Student/BrowseCourses";
+import CourseDetails from "./components/Student/CourseDetails";
+import Assignments from "./components/Student/Assignments";
+import Announcements from "./components/Student/Announcements";
 import InstructorDashboard from "./pages/Dashboard/InstructorDashboard";
 import AdminDashboard from "./pages/Dashboard/AdminDashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
@@ -16,6 +20,7 @@ const App = () => {
                 <Route path="/register" element={<Register />} />
 
                 {/* Protected Routes */}
+                {/* Student Pages */}
                 <Route
                     path="/student-dashboard"
                     element={
@@ -25,6 +30,40 @@ const App = () => {
                     }
                 />
                 <Route
+                    path="/browse-courses"
+                    element={
+                        <ProtectedRoute role="student">
+                            <BrowseCourses />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="/course-details/:courseId"
+                    element={
+                        <ProtectedRoute role="student">
+                            <CourseDetails />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="/assignments/:courseId"
+                    element={
+                        <ProtectedRoute role="student">
+                            <Assignments />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="/announcements/:courseId"
+                    element={
+                        <ProtectedRoute role="student">
+                            <Announcements />
+                        </ProtectedRoute>
+                    }
+                />
+
+                {/* Instructor Pages */}
+                <Route
                     path="/instructor-dashboard"
                     element={
                         <ProtectedRoute role="instructor">
@@ -32,6 +71,8 @@ const App = () => {
                         </ProtectedRoute>
                     }
                 />
+
+                {/* Admin Pages */}
                 <Route
                     path="/admin-dashboard"
                     element={
