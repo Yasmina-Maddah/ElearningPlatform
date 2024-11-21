871adf96b7e69e36c963e4a165c187c9cb1fe1c9
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-21 11:32:34 +0200

    fetched the enrollment service api

---
+++ src/api/enrollmentService.js
@@ -0,0 +1,18 @@
+import api from "./api"; 
+
+export const enrollInCourse = async (courseId) => {
+    const response = await api.post("/enrollments/enroll.php", { courseId });
+    return response.data;
+};
+
+export const getEnrolledStudents = async (courseId) => {
+    const response = await api.get(`/enrollments/getEnrollments.php?courseId=${courseId}`);
+    return response.data;
+};
+
+export const removeEnrollment = async (courseId, studentId) => {
+    const response = await api.delete("/enrollments/removeEnrollment.php", {
+        data: { courseId, studentId },
+    });
+    return response.data;
+};
